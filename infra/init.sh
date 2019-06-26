e950a0daa1e88a0d62fc03e7ee1a1387fe907a4c
Stuart Charlton <scharlton@pivotal.io>
2019-06-26 08:59:07 -0400

    tweaks

--- infra/init.sh
+++ infra/init.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
+set -xu
+
 git submodule foreach --recursive git submodule sync && git submodule update --init --recursive
 
 cd kubo-deployment/manifests
@@ -46,17 +48,17 @@ bosh -n deploy ./cfcr.yml \
  -v pod_network_cidr=10.200.0.0/16 \
  -v first_ip_of_service_cluster_cidr=10.100.200.1
 
-bosh -n run-errand apply-specs &
 
 if [[ ! $(dig $CLUSTER_API A +short) ]]; then
 	MASTER_VM=$(bosh vms | grep master | cut -f5)
+        bosh -n run-errand apply-specs &
 
 	gcloud compute addresses create jupyterhub-$CLUSTER_HOSTNAME-mip --region us-west1
 	gcloud compute target-pools create jupyterhub-$CLUSTER_HOSTNAME-mpool --region us-west1
 	gcloud compute target-pools add-instances jupyterhub-$CLUSTER_HOSTNAME-mpool --instances $MASTER_VM --region us-west1
-	gcloud compute forwarding-rules create jupyterhub-$CLUSTER_HOSTNAME-mrule --region us-west1 --ports 8443 --address jupyterhub-$CLUSTER_HOSTNAME-mip --target-pool jupypter-$CLUSTER_HOSTNAME--mpool
+	gcloud compute forwarding-rules create jupyterhub-$CLUSTER_HOSTNAME-mrule --region us-west1 --ports 8443 --address jupyterhub-$CLUSTER_HOSTNAME-mip --target-pool jupypter-$CLUSTER_HOSTNAME-mpool
 	PUBLIC_MASTER_IP=$(gcloud compute forwarding-rules describe jupyterhub-$CLUSTER_HOSTNAME-mrule --region us-west1 | grep IPAddress | cut -d" " -f2)
 	gcloud dns record-sets transaction start -z k8sycf
-	gcloud dns record-sets transaction add -z k8sycf --name "$CLUSTER_API" --ttl "300" --type="A" "34.83.254.103"
+	gcloud dns record-sets transaction add -z k8sycf --name "$CLUSTER_API" --ttl "300" --type="A" "$PUBLIC_MASTER_IP"
 	gcloud dns record-sets transaction execute -z k8sycf
 fi
