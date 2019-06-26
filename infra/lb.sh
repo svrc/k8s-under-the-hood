7f550a25d4b6572d7096a1fd60e57905ceccef4b
Stuart Charlton <scharlton@pivotal.io>
2019-06-26 11:23:02 -0400

    ttl

--- infra/lb.sh
+++ infra/lb.sh
@@ -14,7 +14,7 @@ if [[ ! $(dig $CLUSTER_API A +short) ]]; then
         gcloud compute forwarding-rules create jupyterhub-$CLUSTER_HOSTNAME-mrule --region us-west1 --ports 8443 --address jupyterhub-$CLUSTER_HOSTNAME-mip --target-pool jupyterhub-$CLUSTER_HOSTNAME-mpool
         PUBLIC_MASTER_IP=$(gcloud compute forwarding-rules describe jupyterhub-$CLUSTER_HOSTNAME-mrule --region us-west1 | grep IPAddress | cut -d" " -f2)
         gcloud dns record-sets transaction start -z k8sycf
-        gcloud dns record-sets transaction add -z k8sycf --name "$CLUSTER_API" --ttl "300" --type="A" "$PUBLIC_MASTER_IP"
+        gcloud dns record-sets transaction add -z k8sycf --name "$CLUSTER_API" --ttl "60" --type="A" "$PUBLIC_MASTER_IP"
         gcloud dns record-sets transaction execute -z k8sycf
         ~/materials/infra/kubo-deployment/bin/set_kubeconfig p-bosh/$BOSH_DEPLOYMENT https://$CLUSTER_API:8443
 fi
