16caaaa37d8498928d937fcd16f721b107771765
Stuart Charlton <scharlton@pivotal.io>
2019-06-26 10:19:36 -0400

    split

--- infra/init.sh
+++ infra/init.sh
@@ -27,5 +27,23 @@ EOF
 
 bosh -n update-config --name=$BOSH_DEPLOYMENT-cc --type=cloud ./$BOSH_DEPLOYMENT-cc.yml
 
-nohup ~/materials/infra/deploy.sh  &
-
+bosh -n deploy ./cfcr.yml \
+ -o ops-files/misc/scale-to-one-az.yml \
+ -o ops-files/add-hostname-to-master-certificate.yml \
+ -o ops-files/change-cidrs.yml \
+-o ops-files/master-kubelet.yml \
+-o ops-files/disable-flannel-enable-ipam.yml \
+-o ops-files/allow-privileged-containers.yml \
+-o ops-files/use-vm-extensions.yml  \
+-o ops-files/cni/calico.yml  \
+-o ops-files/misc/deployment-name.yml \
+-o ops-files/vm-types.yml \
+-v api-hostname=$CLUSTER_API \
+ -v master_vm_type=small \
+ -v worker_vm_type=large \
+ -v apply_addons_vm_type=micro \
+ -v deployment_name=$BOSH_DEPLOYMENT \
+ -v kubedns_service_ip=10.100.200.2 \
+ -v service_cluster_cidr=10.100.200.0/24 \
+ -v pod_network_cidr=10.200.0.0/16 \
+ -v first_ip_of_service_cluster_cidr=10.100.200.1 &
