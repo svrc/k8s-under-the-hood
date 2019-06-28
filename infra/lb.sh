709066615fa00f96d5635ad0786fdae57e2ac846
Stuart Charlton <scharlton@pivotal.io>
2019-06-28 16:21:46 -0400

    lb

--- infra/lb.sh
+++ infra/lb.sh
@@ -20,12 +20,26 @@ done
         gcloud compute target-pools add-instances jupyterhub-$CLUSTER_HOSTNAME-mpool --instances $MASTER_VM --instances-zone us-east1-b
         gcloud compute forwarding-rules create jupyterhub-$CLUSTER_HOSTNAME-mrule --region us-east1 --ports 8443 --address jupyterhub-$CLUSTER_HOSTNAME-mip --target-pool jupyterhub-$CLUSTER_HOSTNAME-mpool
         PUBLIC_MASTER_IP=$(gcloud compute forwarding-rules describe jupyterhub-$CLUSTER_HOSTNAME-mrule --region us-east1 | grep IPAddress | cut -d" " -f2)
-        for i in {0..5}; do
-         gcloud dns record-sets transaction start -z k8sycf
-         gcloud dns record-sets transaction add -z k8sycf --name "$CLUSTER_API" --ttl "60" --type="A" "$PUBLIC_MASTER_IP"
-         gcloud dns record-sets transaction execute -z k8sycf
-         if [  $? -eq 0 ]; then
-          break
-         fi
-         sleep 1
-       done
+        OLD_IP=$(gcloud dns record-sets list -z k8sycf  | grep $CLUSTER_API | awk '{print $4}')
+        if [ ! $PUBLIC_MASTER_IP == $OLD_IP ]; then
+           for i in {0..5}; do
+             gcloud dns record-sets transaction start -z k8sycf
+             gcloud dns record-sets transaction remove -z k8sycf --name "$CLUSTER_API" --ttl "60" --type="A" "$OLD_IP"
+             gcloud dns record-sets transaction add -z k8sycf --name "$CLUSTER_API" --ttl "60" --type="A" "$PUBLIC_MASTER_IP"
+             gcloud dns record-sets transaction execute -z k8sycf
+             if [  $? -eq 0 ]; then
+              break
+             fi
+             sleep 1
+            done
+        else
+          for i in {0..5}; do
+           gcloud dns record-sets transaction start -z k8sycf
+           gcloud dns record-sets transaction add -z k8sycf --name "$CLUSTER_API" --ttl "60" --type="A" "$PUBLIC_MASTER_IP"
+           gcloud dns record-sets transaction execute -z k8sycf
+           if [  $? -eq 0 ]; then
+            break
+           fi
+           sleep 1
+         done
+       fi
